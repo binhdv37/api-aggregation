9f4175e376151740387a42aadb688dc030715fe8
binhdv <binh.doan@sunteco.io>
2022-08-04 11:28:16 +0700

    feat: get swagger data to js object

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('asdwqe');
+import {AxiosResponse} from "axios";
+import * as https from "https";
+
+const axiosRq = require('axios');
+const axios = axiosRq.create({ // by pass ssl err
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+    })
+});
+
+const apiEndpoint = 'https://kong-dev.apps.ocp-eco02.dev.sunteco.local/sun-monitor/api/v2/api-docs';
+
+axios
+    .get(apiEndpoint)
+    .then((res: AxiosResponse<any>) => {
+        console.log('--- SUCCESS ---');
+        console.log(res.data);
+    })
+    .catch((error: any) => {
+        console.log('--- ERROR ---')
+        console.error(error);
+    });
